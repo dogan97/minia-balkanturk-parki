8a51ffe528559f98c62c0a1e91b4d0f58cfc11da
Dogan Esen <doganesen97@gmail.com>
2025-10-06 17:35:46 +0300

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/minia-balkanturk-parki/',
+      base: process.env.NODE_ENV === 'production' ? '/minia-balkanturk-parki/' : '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
